9d05e9c2bd78408327cbe57a00bddc5c094107bf
rehanhajee <rehanhajee@gmail.com>
2018-04-03 15:09:20 -0400

    Compressed populateList method

--- Finances.java
+++ Finances.java
@@ -2,14 +2,14 @@ import java.util.*;
 
 public class Finances {
 	
-	private static ArrayList<Member> allMembers;
-	private static ArrayList<Member> unpaidMembers;
+	private ArrayList<Member> allMembers;
+	private ArrayList<Member> unpaidMembers;
 	// Actual discount proportion, to be calculated as multiple of original cost
 	private float discount = 0.9f;
 	// Number of meetings attended in a row until the member is eligible for a one time discount
-	private static int attendenceForDiscount = 12;
+	private int attendenceForDiscount = 12;
 	// Number of people awarded loyalty discount
-	private static int loyaltyDiscountPeople = 10;
+	private final int LOYALTY_DISCOUNT = 10;
 	
 	// Test method, remove when necessary
 	public static void main (String[] args) {
@@ -20,49 +20,16 @@ public class Finances {
 	
 	// Test method, remove when necessary
 	private static void PopulateList(ArrayList<Member> allMembers) {
-		Member m = new Member();
-		Member n = new Member();
-		Member o = new Member();
-		Member p = new Member();
-		Member q = new Member();
-		Member r = new Member();
-		Member s = new Member();
-		Member t = new Member();
-		Member u = new Member();
-		Member v = new Member();
-		Member w = new Member();
-		Member x = new Member();
-		Member y = new Member();
-		Member z = new Member();
-		m.setTimesAttended(5);
-		n.setTimesAttended(8);
-		o.setTimesAttended(2);
-		p.setTimesAttended(12);
-		q.setTimesAttended(7);
-		r.setTimesAttended(4);
-		s.setTimesAttended(3);
-		t.setTimesAttended(9);
-		u.setTimesAttended(8);
-		v.setTimesAttended(11);
-		w.setTimesAttended(12);
-		x.setTimesAttended(10);
-		y.setTimesAttended(6);
-		z.setTimesAttended(5);
-		allMembers.add(m);
-		allMembers.add(n);
-		allMembers.add(o);
-		allMembers.add(p);
-		allMembers.add(q);
-		allMembers.add(r);
-		allMembers.add(s);
-		allMembers.add(t);
-		allMembers.add(u);
-		allMembers.add(v);
-		allMembers.add(w);
-		allMembers.add(x);
-		allMembers.add(y);
-		allMembers.add(z);
+		Member memberArray[] = new Member[14];
+		String memberName[] = {"m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+		int memberTimesAttended[] = {5, 8, 2, 12, 7, 4, 3, 9, 8, 11, 12, 10, 6, 5};
 		
+		for (int i = 0; i < memberArray.length;i++) {
+			memberArray[i] = new Member();
+			memberArray.setName(memberName[i]);
+			memberArray[i].setTimesAttended(memberTimesAttended[i]);
+		}
+		allMembers.add(memberArray);
 	}
 	
 	// Get all members who have attended more than they have paid
@@ -90,9 +57,10 @@ public class Finances {
 	
 	// Get loyalty discount
 	private static void loyaltyAward(ArrayList<Member> allMembers) {
-		Collections.sort(allMembers, Collections.reverseOrder());
-		int count = loyaltyDiscountPeople;
-		for (int i = 0; i < count; i++) {
+		//Collections.sort(allMembers, Collections.reverseOrder());
+		Collections.sort(attended, new CompareTimesAttended());
+		//int count = loyaltyDiscountPeople;
+		for (int i = 0; i < LOYALTY_DISCOUNT; i++) {
 			// Account for people who attend the same number of times
 			if (i > 1 && allMembers.get(i).getTimesAttended() == allMembers.get(i-1).getTimesAttended()) {
 				count++;
