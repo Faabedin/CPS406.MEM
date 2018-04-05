ed3f3d27653a076a7ff605312bb65508ee613e87
rehanhajee <rehanhajee@gmail.com>
2018-04-04 21:55:50 -0400

    Rewrote Lesson with getters and setters

--- Lesson.java
+++ Lesson.java
@@ -1,4 +1,44 @@
-public class Lesson {
-	private int month;
+class Lesson {
+    private String message;
+    private int month, date, year;
 
+
+    public Lesson(String message, int month, int date, int year) {
+        this.message = message;
+        this.month = month;
+        this.date = date;
+        this.year = year;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getYear() {
+        return year;
+    }
 }
