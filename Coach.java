4414a342e63231b21776a4e56046944b6daf792f
rehanhajee <rehanhajee@gmail.com>
2018-04-04 22:05:08 -0400

    Corrected addMember

--- Coach.java
+++ Coach.java
@@ -7,8 +7,8 @@ public class Coach {
 	private String name, className;
 	private Member member;
 	private Lesson lesson;
-	private ArrayList MemberList;
-	private ArrayList NotPaidList;
+	private ArrayList memberList;
+	private ArrayList notPaidList;
 	private ArrayList lessonList; //Might be moved to another class
 
 	//public Coach(){};
@@ -16,15 +16,17 @@ public class Coach {
 	public Coach(String name, String className){
 		this.name = name;
 		this.className = className;
-		MemberList = new LinkedList();
+		memberList = new ArrayList<>();
 	}
 	public void addMember(String name, String phoneNumber, boolean paid, String address){
-		member = new Member(name,phoneNumber,paid,address);
-		MemberList.add(member);
+		member = new Member(name, phoneNumber, paid, address);
+		/*MemberList.add(member);
 		
 		if (!member.isPaid()){
 			NotPaidList.add(member);
-		}
+		}*/
+		MemberList.add(member);
+		if (!paid) notPaidList.add(member);
 	}
 	public void scheduleLesson(int month, int date, int year, String message){
 		// TODO implement Lesson
