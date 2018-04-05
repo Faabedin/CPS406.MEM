cd42569dc8a278ab7595a134ed1644d17e97aa1b
Akash Chhabria <akashrai10.ar10@gmail.com>
2018-04-05 17:43:04 -0400

    login and member section

---
+++ MemberController.java
@@ -0,0 +1,24 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import javax.xml.soap.Text;
+import java.awt.*;
+import java.awt.TextArea;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.scene.control.*;
+import javafx.scene.control.Label;
+
+
+public class MemberController {
+
+    @FXML
+    private Label label;
+
+    @FXML
+    public void initialize() {
+        label.setText("member");
+    }
+}
