d0b0be37ddff429cc640ed3749c10fc99975548b
rehanhajee <rehanhajee@gmail.com>
2018-04-05 23:50:41 -0400

    Added a temporary main to move code to GUI

--- Login.java
+++ Login.java
@@ -1,5 +1,29 @@
 import java.util.*;
 
+public static void main(String argz[]) {
+        Map<String, String> loginList = new Login().getLoginMap();
+        Scanner input = new Scanner(System.in); //remove when reading from button
+        System.out.print("Username? "); //remove when reading from button
+        String username = input.next(); //textField.getText() or whatever
+        System.out.print("Password? "); //remove when reading from button
+        String password = input.next(); //textField.getText() or whatever
+
+        Iterator iterator = loginList.entrySet().iterator();
+        if (loginList.containsKey(username)) {
+            Map.Entry entry;
+            while (iterator.hasNext()) {
+                for (int i = 0; i < loginList.size(); i++) {
+                    entry = (Map.Entry) iterator.next();
+                    if (entry.getKey().equals(username) && entry.getValue().equals(password)) {
+                        System.out.println("Correct login."); //change to whatever
+                    } else {
+                        System.out.println("Incorrect login."); //change to whatever
+                    }
+                }
+            }
+        }
+    }
+
 class Login{
 	/*
 	int loginNumber;
