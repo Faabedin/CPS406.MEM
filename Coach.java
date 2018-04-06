6eff045c20a12e0e8ed4288812b279915b5af78c
rehanhajee <rehanhajee@gmail.com>
2018-04-06 16:42:49 -0400

    Removed unneeded comments

--- Coach.java
+++ Coach.java
@@ -1,17 +1,18 @@
 import java.util.ArrayList;
 
+/**
+ *
+ *
+ */
+
 public class Coach {
-	/*
-	 * 
-	 */
+	
 	private String name, className;
 	private Member member;
 	private Lesson lesson;
 	private ArrayList memberList;
 	private ArrayList notPaidList;
 	private ArrayList lessonList; //Might be moved to another class
-
-	//public Coach(){};
 	
 	public Coach(String name, String className){
 		this.name = name;
@@ -22,23 +23,14 @@ public class Coach {
 	}
 	public void addMember(String name, String phoneNumber, boolean paid, String address){
 		member = new Member(name, phoneNumber, paid, address);
-		/*MemberList.add(member);
-		
-		if (!member.isPaid()){
-			NotPaidList.add(member);
-		}*/
 		MemberList.add(member);
 		if (!paid) notPaidList.add(member);
 	}
 	public void scheduleLesson(int month, int date, int year, String message){
-		// TODO implement Lesson
-		// lesson = new Lesson(month,date,year,message);
-		//lessonList.add(lesson);
 		lessonList.add(new Lesson(month, date, year, message);
 	}
 	public void cancelClass(int month, int date, int year, String message ){
 		if (lessonList.contains(new Lesson(month, date, year, message))) 
 			lessonList.remove(new Lesson(month, date, year, message));
 	}
-	
 }
