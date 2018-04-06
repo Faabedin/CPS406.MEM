8b0f3967cda62717d014ddf377e33472a1886500
rehanhajee <rehanhajee@gmail.com>
2018-04-05 23:38:15 -0400

    User and Pass list can now be read from file

--- Login.java
+++ Login.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
-public class Login{
+class Login{
+	/*
 	int loginNumber;
 	String Password;
 	
@@ -10,5 +11,36 @@ public class Login{
 	
 	}
 	
-	}	
+	}*/
+	
+	private Map<String,String> loginMap;
+
+	Login() {
+		loginMap = new TreeMap<>();
+		try {
+		    readLoginFile();
+		} catch (Exception e) {
+		    System.out.println("There was a problem reading the file. The login list has not been created.");
+		}
+	}
+
+    private void readLoginFile() throws IOException {
+
+        Scanner fileScanner = new Scanner(new File("loginPass.txt");
+
+        while (fileScanner.hasNext()) {
+            try {
+                loginMap.put(fileScanner.next(), fileScanner.next());
+            } catch (Exception ex) {
+                System.out.println("There was a problem reading the file. The login list has not been created.");
+                loginMap = new TreeMap<>();
+                break;
+            }
+        }
+        fileScanner.close();
+    }
+
+    public Map<String, String> getLoginMap() {
+        return loginMap;
+    }
 }
