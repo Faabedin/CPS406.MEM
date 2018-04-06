61e9911729842bf08a0bbb1152955841baa2ea4f
rehanhajee <rehanhajee@gmail.com>
2018-04-06 14:12:23 -0400

    Added IO import

--- Main.java
+++ Main.java
@@ -5,6 +5,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.File;
+
 public class Main extends Application throws IOException {
 
     @Override
