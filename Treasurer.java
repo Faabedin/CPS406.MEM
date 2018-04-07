71bc54c130443fd19ac38be3d256862dfd315145
faizaabedin <33582390+faizaabedin@users.noreply.github.com>
2018-04-06 20:03:18 -0400

    added createCoach(name) and removeCoach(name)

--- Treasurer.java
+++ Treasurer.java
@@ -1,81 +1,103 @@
+
+
 import java.util.LinkedList;
 import java.util.ArrayList;
+
 
 /**
  * 	incomestatement - revenue(member payment ) + expense(coach’s payments, hall expenses) 
  *member payment - (length of members - length of notPaid list) * (monthly payment amount) 
  */
-class Treasurer {
+public class Treasurer {
 	int memberPayment,revenue;
 	int coachPayment,hallExpense,expense;
-	//int netIncome;
-	//Linkedlist<int> ll;
+	int netIncome;
+	Coach coach;
+	Arraylist coachList;
 	ArrayList<Integer> ll;
 
 
-	Treasurer(){
+	public Treasurer(){
 		memberPayment = 3000;// memberList.length() - memberNotPaidList.length()
  		coachPayment = 600;
 		hallExpense = 1500;
 		revenue = memberPayment;
 		expense = coachPayment + hallExpense;
-		//netIncome = revenue - expense;
+		netIncome = revenue - expense;
 		//ll = new Linkedlist<int>();
 		ll = new ArrayList<>();
-	}
-	
-	public void setMemberPayment(int memberPayment) {
-		this.memberPayment = memberPayment;
-	}
-	
-	public void setCoachPayment(int coachPayment) {
-		this.coachPayment = coachPayment;
-	}
-	
-	public void setHallExpense(int hallExpense) {
-		this.hallExpense = hallExpense;
-	}
-	
-	public void setExpense(int expense) {
-		this.expense = expense;
-	}
-	
-	public void setRevenue(int revenue) {
-		this.revenue = revenue;
 	}
 	
 	public int getMemberPayment() {
 		return memberPayment;
 	}
 
+	public void setMemberPayment(int memberPayment) {
+		this.memberPayment = memberPayment;
+	}
+
 	public int getCoachPayment() {
 		return coachPayment;
+	}
+
+	public void setCoachPayment(int coachPayment) {
+		this.coachPayment = coachPayment;
 	}
 
 	public int getHallExpense() {
 		return hallExpense;
 	}
 
+	public void setHallExpense(int hallExpense) {
+		this.hallExpense = hallExpense;
+	}
+
 	public int getRevenue() {
 		return revenue;
+	}
+
+	public void setRevenue(int revenue) {
+		this.revenue = revenue;
 	}
 
 	public int getExpense() {
 		return expense;
 	}
 
+	public void setExpense(int expense) {
+		this.expense = expense;
+	}
+	
+	public int getNetIncome() {
+		return netIncome;
+	}
+
+	public void setNetIncome(int netIncome) {
+		this.netIncome = netIncome;
+	}
+	public void createCoach(String name){
+		Coach coach = new Coach(name);
+		coachList.add(coach);
+	}
+	public void removeCoach(String name){
+		Coach coach = new Coach(name);
+		if(coachList.contains(coach)){
+			coachList.remove(coach)
+		}
+	}
 
 	public String incomeStatement(){
-		int totalExpenses = coachPayment + hallExpense;
-		int profit = revenue - totalExpenses;
-		return "INCOME STATEMENT "+ "\n"+
-				"Revenues" + "\n" +
-				"\t" + "Member Payment" + "\t\t$" + memberPayment + "\n" + 
-				"Operating Expenses" + "\n" +
-				"\t" + "Coach Payment" + "\t$" + coachPayment + "\n" +
-				"\t" + "Hall Expense" + "\t$" + hallExpense + "\n" +
-				"Total operating Expense" + "\t\t$" + totalExpenses + "\n" + 
-				"Profit" + "\t\t\t\t$" + profit;
+		ll.toString();
+		return "INCOME STATEMENT "+ '\n'+
+				"Revenue:" + '\n' +
+				'\t' + "Member Payment: " + '\t' + memberPayment + '\n' + 
+				"Expenses:" + '\n' +
+				'\t' + "Coach Payment: " + '\t' + coachPayment + '\n' +
+				'\t' + "Hall Expense: " +'\t' + hallExpense + '\n' +
+				 '\n' +
+				"Net Income" +'\t' + '\t' + netIncome;
 	}
+	
+	
 }
 
